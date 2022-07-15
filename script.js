aa6ce0480347910ef7aa3f58ff20eb33fd8e3552
Zupi-HR <49796750+Zupi-HR@users.noreply.github.com>
2022-07-15 08:23:00 +0200

    finished game function
    waiting for discord review

--- script.js
+++ script.js
@@ -16,27 +16,42 @@ function computerPlay() {
 }
 
 const playerSelection = prompt("Your turn:");
-const computerSelection = computerPlay();
+let computerSelection;
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection.toLowerCase() === "rock") && (computerSelection.toLowerCase() === "scissors")) {
+        playerScore++;
         return `You win!, Rock beats Scissors`;
     } else if ((playerSelection.toLowerCase() === "paper") && (computerSelection.toLowerCase() === "rock")) {
+        playerScore++;
         return `You win! Paper beats Rock`;
     } else if ((playerSelection.toLowerCase() === "scissors") && (computerSelection.toLowerCase() === "paper")) {
+        playerScore++;
         return `You win! Scissors beats Paper`;
     } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return `It's a draw! ${playerSelection} and ${computerSelection} are same`;
     } else {
+        computerScore++;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 
 }
 
 function game() {
-    for (i = 0; i < 5; i++) {  
+    for (i = 0; i < 5; i++) {
+        computerSelection = computerPlay();
         const game = playRound(playerSelection, computerSelection);
         console.log(game);
+    }
+    if (playerScore > computerScore) {
+        console.log(`Your score is: ${playerScore}, computer score is: ${computerScore}, so you win!`)
+    } else if (computerScore > playerScore) {
+        console.log(`Your score is: ${playerScore}, computer score is: ${computerScore}, so computer win!`);
+    } else {
+        console.log(`Your score is: ${playerScore}, computer score is: ${computerScore}, so it's a draw!`);
     }
 }
 
